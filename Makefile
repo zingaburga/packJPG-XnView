bb995bb9b16afc96a68f9c91f7df86eafe6c321c
zingaburga <zingaburga@zingaburga.com>
2013-11-09 22:11:48 +1000

    Remove libjpeg-turbo from plugin and switch to using new experimental libyuv method

--- Makefile
+++ Makefile
@@ -3,17 +3,18 @@ XNVIEW_PLUGIN = xnview/Xpjg.usr
 PACKJPG_LIB = packJPG/bitops.o packJPG/aricoder.o packJPG/packjpg.o
 #PACKJPG_LIB = packJPG/packJPGlib.a #dunno why it doesn't work
 LIBJPEG_LIB = libjpeg-turbo/.libs/libjpeg.a
+LIBYUV_LIB = libyuv/libyuv.a
 
-DEPS = $(PACKJPG_LIB) xnview/xpjg.o $(LIBJPEG_LIB)
+DEPS = $(PACKJPG_LIB) xnview/xpjg.o $(LIBJPEG_LIB) $(LIBYUV_LIB)
 CFLAGS = -Wall -Wl,--kill-at -s -O2 -shared -static-libgcc -static-libstdc++
-CPPFLAGS = $(CFLAGS) -o $(XNVIEW_PLUGIN) $(DEPS)
+CPPFLAGS = $(CFLAGS) -Ilibyuv/include -o $(XNVIEW_PLUGIN) $(DEPS)
 
 $(XNVIEW_PLUGIN): $(DEPS)
 	g++ $(CFLAGS) -o $@ $^
 
 xnview/xpjg.o: xnview/xpjg.c
 	cd xnview
-	gcc -Wall -march=i686 -O3 -o $@ -c $^
+	gcc -Ilibyuv/include -Wall -march=i686 -O3 -o $@ -c $^
 
 fprofile-use:
 	cd packJPG && make --makefile=Makefile_dll_fprofile_use
@@ -32,3 +33,6 @@ $(LIBJPEG_LIB):
 	echo "Please build libjpeg"
 	# built with:
 	# export CFLAGS="-march=i686" && ./configure --with-jpeg8 && make
+
+$(LIBYUV_LIB):
+	cd libyuv && make
--- xnview/xpjg.c
+++ xnview/xpjg.c
@@ -6,24 +6,16 @@
 
 #include <stdio.h>
 #include <windows.h>
-#include <setjmp.h>
 
 
 /*** library includes ***/
-
-// needed for libjpeg
-#define XMD_H
-#define HAVE_BOOLEAN
-#define INT16 short
-#define INT32 long
-#define boolean int
-#include "../libjpeg-turbo/jpeglib.h"
 
 #ifndef __cplusplus
 typedef int bool;
 #endif
 #include "../packJPG/packjpglib.h"
 
+#include "libyuv.h"
 
 
 #ifdef __cplusplus
@@ -69,91 +61,36 @@ DLLEXPORT BOOL API gfpGetPluginInfo( DWORD version, LPSTR label, INT label_max_s
 }
 
 
-/*** libjpeg stuff ***/
-
-struct my_error_mgr {
-  struct jpeg_error_mgr pub;	/* "public" fields */
-
-  jmp_buf setjmp_buffer;	/* for return to caller */
-};
-
-typedef struct my_error_mgr * my_error_ptr;
-
 typedef struct {
-		unsigned char * jpegFile;
-		unsigned int jpegLen;
-		struct jpeg_decompress_struct cinfo;
-		struct my_error_mgr jerr;
-		JSAMPARRAY jbuffer;
+		unsigned char * rgbData;
+		unsigned int dataLen;
 		int decodeStatus; // 0 = not started, 1 = started, 2 = finished
-	} MYDATA; 
-
-METHODDEF(void) libjpeg_error_exit (j_common_ptr cinfo)
-{
-	/* cinfo->err really points to a my_error_mgr struct, so coerce pointer */
-	my_error_ptr myerr = (my_error_ptr) cinfo->err;
-	char msg[512], msgTitle[256];
-	
-	if(cinfo->err->msg_code != 55) // don't display 'not a JPEG' error
-	{
-		vsprintf(msg, cinfo->err->jpeg_message_table[cinfo->err->msg_code], cinfo->err->msg_parm.s);
-		sprintf(msgTitle, "JPEG Error (%d)", cinfo->err->msg_code);
-		MessageBox(NULL, msg, msgTitle, 0); // TODO: get active window hWnd?
-	}
-
-	/* Return control to the setjmp point */
-	longjmp(myerr->setjmp_buffer, 1);
-}
-DLLEXPORT void API gfpLoadPictureExit( void * ptr );
-static BOOL inline libjpeg_error(MYDATA* data)
-{
-	if (setjmp(((my_error_ptr)(data->cinfo.err))->setjmp_buffer)) {
-		jpeg_abort((j_common_ptr)&(data->cinfo));
-		// TODO: prevent crash?
-		//jpeg_destroy_decompress(cinfo);
-		//gfpLoadPictureExit(data); // - XnView will call this for us?
-		return FALSE;
-	}
-	return TRUE;
-}
-
-
+		
+		unsigned int stride;
+		//unsigned int width, height;
+		//unsigned int bpp;
+		struct pjglib_pjg_info info;
+		unsigned char * imageStream;
+		
+		void* pjg;
+	} MYDATA;
 
 DLLEXPORT void * API gfpLoadPictureInit( LPCSTR filename )
 {
-	MYDATA * data; 
-	void* pjg;
-	data = (MYDATA*)calloc( 1, sizeof(MYDATA) ); 
-
+	MYDATA * data = (MYDATA*)calloc( 1, sizeof(MYDATA) ); 
+	
 	#define ERROR_EXIT { free(data); return NULL; }
-	if(!(pjg = pjglib_get_instance()))
+	if(!(data->pjg = pjglib_get_instance()))
 		ERROR_EXIT
-	pjglib_init_streams(pjg, (void*)filename, 0, 0, &(data->jpegFile), 1);
-	if(!pjglib_convert_stream2mem(pjg, &(data->jpegFile), &data->jpegLen, NULL))
-		ERROR_EXIT
-	pjglib_del_instance(pjg);
+	pjglib_init_streams(data->pjg, (void*)filename, 0, 0, &data->imageStream, 1);
 	
-	if(!data->jpegLen)
+	if(!pjglib_pjg_get_info(data->pjg, &data->info))
 		ERROR_EXIT
 	
-	// packJPG process done, now do JPEG decode
-	{
-		// init JPEG
-		struct jpeg_decompress_struct * cinfo = &(data->cinfo);
-		struct my_error_mgr * jerr = &(data->jerr);
-		cinfo->err = jpeg_std_error(&(jerr->pub));
-		jerr->pub.error_exit = libjpeg_error_exit;
-		/* Establish the setjmp return context for my_error_exit to use. */
-		if(!libjpeg_error(data)) {
-			// need to specifically clean up here
-			jpeg_destroy_decompress(cinfo);
-			free(data->jpegFile); 
-			free(data); 
-			return NULL;
-		}
-		jpeg_create_decompress(cinfo);
-		jpeg_mem_src(cinfo, data->jpegFile, data->jpegLen);
-	}
+	// TODO: align stride??
+	data->stride = data->info.width*3;
+	data->dataLen = data->stride * data->info.height;
+	
 	return data; 
 }
 
@@ -161,22 +98,14 @@ DLLEXPORT void * API gfpLoadPictureInit( LPCSTR filename )
 DLLEXPORT BOOL API gfpLoadPictureGetInfo( void * ptr, INT * pictype, INT * width, INT * height, INT * dpi, INT * bits_per_pixel, INT * bytes_per_line, BOOL * has_colormap, LPSTR label, INT label_max_size )
 {
 	MYDATA * data = (MYDATA *)ptr; 
-	struct jpeg_decompress_struct * cinfo = &(data->cinfo);
-	if(!libjpeg_error(data)) return FALSE;
-	jpeg_read_header(cinfo, TRUE);
-	
-	// set decompression options
-	cinfo->out_color_space = JCS_RGB; // TODO: check if correct
-	jpeg_calc_output_dimensions(cinfo);
-	
 	
 	// fill out info
-	*pictype = GFP_RGB; 
-	*width = cinfo->output_width;
-	*height = cinfo->output_height;
+	*pictype = GFP_BGR; 
+	*width = data->info.width;
+	*height = data->info.height;
 	*dpi = 72; // 68
-	*bits_per_pixel = cinfo->output_components*8;
-	*bytes_per_line = *bits_per_pixel/8 * *width; 
+	*bits_per_pixel = data->info.channels*8; // TODO: change
+	*bytes_per_line = 3 * *width; 
 	*has_colormap = FALSE; 
 	strncpy( label, "packJPG 2.5 file", label_max_size ); 
 	
@@ -186,28 +115,25 @@ DLLEXPORT BOOL API gfpLoadPictureGetInfo( void * ptr, INT * pictype, INT * width
 DLLEXPORT BOOL API gfpLoadPictureGetLine( void * ptr, INT line, unsigned char * buffer )
 {
 	MYDATA * data = (MYDATA *)ptr; 
-	struct jpeg_decompress_struct * cinfo = &(data->cinfo);
 	
 	if(!data->decodeStatus)
 	{
-		if(!libjpeg_error(data)) return FALSE;
-		// set decompression options
-		cinfo->out_color_space = JCS_RGB;
+		unsigned int imageLen;
 		data->decodeStatus = 1;
+		// start decoding
+		if(!pjglib_pjg_get_argb(data->pjg, &data->imageStream, &imageLen, NULL))
+			return FALSE;
 		
-		jpeg_start_decompress(cinfo);
-		data->jbuffer = (*cinfo->mem->alloc_sarray)
-				((j_common_ptr) cinfo, JPOOL_IMAGE, cinfo->output_components * cinfo->output_width, 1);
+		if(!imageLen)
+			return FALSE;
+		
+		// TODO: we could probably do the following line by line
+		data->rgbData = (unsigned char*)malloc(data->dataLen);
+		ARGBToRGB24(data->imageStream, data->info.width*4, data->rgbData, data->stride, data->info.width, data->info.height);
+		// TODO: when is imageStream ever cleared?
 	}
-	
-	if(cinfo->output_scanline != (unsigned int)line) return FALSE;
-	if(cinfo->output_scanline >= cinfo->output_height)
-		return FALSE;
-	//// assume only one buffer
-	data->jbuffer[0] = buffer;
-	jpeg_read_scanlines(cinfo, data->jbuffer, 1);
-	
-	if(cinfo->output_scanline == cinfo->output_height)
+	memcpy(buffer, data->rgbData + data->stride*line, data->stride);
+	if(line+1 == data->info.height)
 		data->decodeStatus = 2;
 	return TRUE; 
 }
@@ -220,20 +146,9 @@ DLLEXPORT BOOL API gfpLoadPictureGetColormap( void * ptr, void * cmap )
 DLLEXPORT void API gfpLoadPictureExit( void * ptr )
 {
 	MYDATA * data = (MYDATA *)ptr; 
-	struct jpeg_decompress_struct * cinfo = &(data->cinfo);
-	if(libjpeg_error(data)) {
-		if(cinfo) {
-			if(data->decodeStatus) {
-				if(data->decodeStatus == 1)
-					jpeg_abort_decompress(cinfo);
-				else
-					jpeg_finish_decompress(cinfo);
-			}
-			jpeg_destroy_decompress(cinfo);
-			cinfo = NULL;
-		}
-	}
-	free(data->jpegFile); 
+	pjglib_del_instance(data->pjg);
+	if(data->decodeStatus != 2) {} // something wrong...
+	free(data->rgbData); 
 	free(data); 
 }
 
